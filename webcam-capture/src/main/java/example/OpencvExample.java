1d0167fb1dc540a5221956a9de34f5e2e1f7291a
Leo Kwon <mtkwnkr@gmail.com>
2020-08-01 02:17:21 +0900

    find contours, draw contours, draw convex hull

--- webcam-capture/src/main/java/example/OpencvExample.java
+++ webcam-capture/src/main/java/example/OpencvExample.java
@@ -7,6 +7,8 @@ import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OpencvExample {
 
@@ -20,7 +22,7 @@ public class OpencvExample {
 
         // finger detection with hsv
         String savePath = "src/main/resources";
-        String imagePath = new File(savePath + "/hand.jpg").getAbsolutePath();
+        String imagePath = new File(savePath + "/gongcha_menu.png").getAbsolutePath();
         // convert source image to HSV
         Mat srcImage = loadImage(imagePath);
         Mat blurImage = new Mat();
@@ -33,7 +35,44 @@ public class OpencvExample {
         Mat maskedImage = new Mat();
         Core.inRange(hsvImage, scalarLower, scalarUpper, maskedImage);
         // save masked image
-        saveImage(maskedImage, savePath + "/hand_detected.jpg");
+        saveImage(maskedImage, savePath + "/gongcha_menu_detected.png");
+
+        // morphological operators
+        // dilate with large element, erode with small ones
+        Mat morphImage = new Mat();
+        Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(12, 12));
+        Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(12, 12));
+        Imgproc.erode(maskedImage, morphImage, erodeElement);
+        Imgproc.erode(maskedImage, morphImage, erodeElement);
+        Imgproc.dilate(maskedImage, morphImage, dilateElement);
+        Imgproc.dilate(maskedImage, morphImage, dilateElement);
+
+        // find contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(morphImage, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+        // draw contours
+        Mat contImage = srcImage.clone();
+        Imgproc.drawContours(contImage, contours,-1, new Scalar(0, 255, 0), 2);
+        saveImage(contImage, savePath + "/gongcha_menu_contour.png");
+
+        // draw convex hull
+        MatOfPoint points = contours.get(0);
+        List<MatOfPoint> hullList = new ArrayList<>();
+        for (MatOfPoint contour : contours) {
+            MatOfInt hull = new MatOfInt();
+            Imgproc.convexHull(points, hull);
+            Point[] contourArray = points.toArray();
+            Point[] hullPoints = new Point[hull.rows()];
+            List<Integer> hullContourIdxList = hull.toList();
+            for (int i = 0; i < hullContourIdxList.size(); i++) {
+                hullPoints[i] = contourArray[hullContourIdxList.get(i)];
+            }
+            hullList.add(new MatOfPoint(hullPoints));
+        }
+        Mat convImage = contImage.clone();
+        Imgproc.drawContours(convImage, hullList, -1, new Scalar(0, 0, 255), 2);
+        saveImage(convImage, savePath + "/gongcha_menu_convex_hull.png");
     }
 
     // load image
